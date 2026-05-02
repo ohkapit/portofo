84c182593b3c71a82073d115595bfe82faf9b5b4
ohkapit <kafitmustofa9@gmail.com>
2026-05-02 13:34:01 +0700

    Update mini-card.js

--- components/mini-card.js
+++ components/mini-card.js
@@ -1,5 +1,5 @@
 export const MiniCard = ({ colorClass, iconClass, label, isCustom = false, customHtml = '' }) => `
-<div class="bg-white dark:bg-gray-800 shadow-md shadow-gray-200 dark:shadow-gray-900 rounded-lg p-3 flex items-center gap-3 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
+<div class="reveal bg-white dark:bg-gray-800 shadow-md shadow-gray-200 dark:shadow-gray-900 rounded-lg p-3 flex items-center gap-3 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
     ${isCustom 
         ? `<div class="w-10 flex justify-center">${customHtml}</div>` 
         : `<div class="text-2xl md:text-3xl ${colorClass} w-10 text-center"><i class="${iconClass}"></i></div>`
